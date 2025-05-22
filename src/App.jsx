489146f5b049279c53b96670da84136352f5cb30
Wellington <w9llington@gmail.com>
2025-05-22 17:32:19 -0300

    setup vite tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+        <div>
+            <h1 className='underline'>Bem vindo ao meu portifólio!</h1>
+        </div>
+    </main>
+  )
+}
+
+export default App
